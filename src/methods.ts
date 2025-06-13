f38446fa357d90822feac91480bd8fa2688bd993
fernandogs04 <fernando.galvao0429@gmail.com>
2025-06-13 08:20:02 -0300

    Permitir que funcione em todos os tipos de terminal

--- src/methods.ts
+++ src/methods.ts
@@ -1,11 +1,27 @@
 import * as vscode from 'vscode';
 
+async function removeFolderByBlob(marker: string): Promise<void> {
+    const files = await vscode.workspace.findFiles(`**/${marker}/**`, '**/node_modules/**');
+
+    const testResultsFiles = new Set(files.map((x) => {
+        var fullPath = x.fsPath;
+        const index = fullPath.indexOf(marker);
+        return index === -1 
+            ? fullPath
+            : fullPath.slice(0, index + marker.length);
+    }));
+
+    testResultsFiles.forEach(async resultPath => {
+        await vscode.workspace.fs.delete(vscode.Uri.file(resultPath), { recursive: true, useTrash: true, });
+    });
+}
+
 export async function deletePastResults(): Promise<vscode.TerminalExitStatus> {
     let terminal = vscode.window.createTerminal();
     terminal.sendText("dotnet tool install -g dotnet-reportgenerator-globaltool");
-    terminal.sendText("powershell rm -r -fo ./coverage_report");
-    terminal.sendText("powershell rm -r -fo ./**/TestResults");
-    terminal.sendText("; exit");
+    await removeFolderByBlob("coverage_report");
+    await removeFolderByBlob("TestResults");
+    terminal.sendText("exit");
     return new Promise<vscode.TerminalExitStatus>((resolve, reject) => {
         const disposeToken = vscode.window.onDidCloseTerminal(async (closedTermianl) => {
             if(closedTermianl === terminal) {
@@ -24,7 +40,7 @@ export async function deletePastResults(): Promise<vscode.TerminalExitStatus> {
 export async function collectTestResults(): Promise<vscode.TerminalExitStatus> {
     let terminal = vscode.window.createTerminal();
     terminal.sendText("dotnet test --collect:\"XPlat Code Coverage\"");
-    terminal.sendText("; exit");
+    terminal.sendText("exit");
     return new Promise<vscode.TerminalExitStatus>((resolve, reject) => {
         const disposeToken = vscode.window.onDidCloseTerminal(async (closedTermianl) => {
             if(closedTermianl === terminal) {
@@ -43,8 +59,8 @@ export async function collectTestResults(): Promise<vscode.TerminalExitStatus> {
 export async function generateReport(): Promise<vscode.TerminalExitStatus> {
     let terminal = vscode.window.createTerminal();
     terminal.sendText("reportgenerator -reports:./**/coverage.cobertura.xml -targetdir:coverage_report -filefilters:-**Moq** -assemblyFilters:-*.Tests");
-    terminal.sendText("./coverage_report/index.html");
-    terminal.sendText("; exit");
+    terminal.sendText("start ./coverage_report/index.html");
+    terminal.sendText("exit");
     return new Promise<vscode.TerminalExitStatus>((resolve, reject) => {
         const disposeToken = vscode.window.onDidCloseTerminal(async (closedTermianl) => {
             if(closedTermianl === terminal) {
